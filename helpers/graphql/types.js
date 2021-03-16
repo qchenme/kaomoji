f848a3c719578445517529e88a2eae9bc019ebe6
Qi <mail@qichen.me>
2021-03-15 22:37:56 -0400

    Add description to graphql types

--- helpers/graphql/types.js
+++ helpers/graphql/types.js
@@ -1,37 +1,69 @@
 import { gql } from "apollo-server-micro";
 
 export const typeDefs = gql`
+  "Queries available."
   type Query {
+    "Get all kaomojis."
     allKaomojis: [String!]!
+    "Get kaomojis in the category specified."
     kaomojiByCategory(category: Category!): KaomojiCategory!
+    "Get a random kaomoji."
     randomKaomoji: String!
   }
+
+  "An object that wraps a category and all kaomojis in this category."
   type KaomojiCategory {
+    "Category these kaomojis belongs to."
     category: Category!
+    "A list of kaomojis."
     kaomojis: [String!]!
   }
+
+  "All categories of kaomojis available."
   enum Category {
+    "Love."
     LOVE
+    "Happy."
     HAPPY
+    "Cool."
     COOL
+    "Dance."
     DANCE
+    "Celebrate."
     CELEBRATE
+    "Sad."
     SAD
+    "Embarrassed."
     EMBARRASSED
+    "Angry."
     ANGRY
+    "Cat."
     CAT
+    "Bear."
     BEAR
+    "Sleep or sleepy."
     SLEEP
+    "Flip table."
     FLIP_TABLE
+    "Judging."
     JUDGING
+    "Expressionless."
     EXPRESSIONLESS
+    "Confused."
     CONFUSED
+    "Unbelievable."
     UNBELIEVABLE
+    "Cheer up."
     CHEER_UP
+    "Bleeding."
     BLEEDING
+    "Run away."
     RUN
+    "Stalk."
     STALK
+    "Yummy."
     YUMMY
+    "Lay down."
     LAY_DOWN
   }
 `;
